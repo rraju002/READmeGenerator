8eca7653d4988f5465a73858e385aa307e2a328d
Rhowen01 <rraju002@ucr.edu>
2022-10-25 10:20:50 -0700

    develop markdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -28,21 +28,71 @@ let licenses = [
 ]
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+  return licenses.filter(l => l.name == license)[0].badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+  return licenses.filter(l => l.name == license)[0].link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  let badge = renderLicenseBadge(license);
+  let link = renderLicenseLink(license);
+
+  let licenseStr = `[![License](${badge})](${link})`;
+  return licenseStr;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let readMeText = '';
+  //title
+  if (data.title) {
+    let title = `# ${data.title}
+    `;
+    readMeText += title;
+  }
+   // Create the TOC
+   let toc = generateToC(data);
 
-`;
+   //add TOC to the README
+   readMeText += descSection;
+
+   //create installation field 
+   let installSection = generateInstructions (data);
+   readMeText += installSection;
+   
+   //create usage section
+   let usageSection = generateUsage(data);
+   readMeText += usageSection;
+
+   //create the contribution section 
+   let contributionSection = generateContribute(data);
+   readMeText += testSection; 
+
+   //create test section 
+   let testSection = generateTestSection(data);
+   readMeText += testSection;
+
+   //create question section 
+   let questionSection = generateQuestions(data); 
+   readMeText += licenseUsed; 
+   return readMeText;
 }
 
 module.exports = generateMarkdown;
